b8b212a797a46a928d49a7f36d0173555b0b4f81
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-09-03 18:44:36 +0530

    first custom component

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,16 +1,19 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
 import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
 
-export default function MenuScreen() {
+const ProductListItem = ({ product }) => {
+  console.log(product)
   return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+    <View style={styles.container}>
+      <Image source={{ uri: product.image}} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
-  );
+  )
 }
+
+export default ProductListItem
 
 const styles = StyleSheet.create({
   container: {
